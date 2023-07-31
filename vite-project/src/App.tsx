f7027ec282fd3497637702e36197d470022da6b0
jobljoel <joelrweaver@gmail.com>
2023-07-30 21:14:16 -0500

    JW-0001: listgroup props added

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,9 +3,11 @@ import ListGroup from "./components/ListGroup";
 import "./App.css";
 
 function App() {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
   return (
     <div>
-      <ListGroup></ListGroup>
+      <ListGroup items={items} heading="Cities"></ListGroup>
     </div>
   );
 }
